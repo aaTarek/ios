31e04501ba10c76d071834e9b0ef0b66f449a6d4
Devmohammed <mohammed.ichou@hotmail.fr>
2021-02-15 18:50:39 +0100

    First Commit

---
+++ Donut/Donut/DonutApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonutApp.swift
+//  Donut
+//
+//  Created by mohammed ichou on 15/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct DonutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
